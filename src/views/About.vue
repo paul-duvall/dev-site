f2190618b26acb799cbb05f5017c6b2476105c1d
Paul Duvall <duvallpj@gmail.com>
2020-05-25 11:35:04 +0100

    feat: start adding basic blog functionality

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <h1>This is a blog page</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.title">
+        {{ post.title }}
+        <p>{{ post.text }}</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import posts from '../assets/posts.json'
+
+export default {
+    name: 'About',
+
+    data: function () {
+    return {
+        posts,
+        test: 'hi'
+    }
+}
+  }
+</script>
