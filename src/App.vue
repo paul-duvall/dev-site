964e8b05638900186381102e6c8ecf8bd8f98dac
Paul Duvall <duvallpj@gmail.com>
2020-05-25 10:28:07 +0100

    Chore: add Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,20 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <AppNavigation />
+    <router-view/>
+  </div>
 </template>
 
+<style lang="scss">
+</style>
+
 <script>
-import HelloWorld from './components/HelloWorld';
+import AppNavigation from '@/components/Navigation.vue'
 
 export default {
-  name: 'App',
-
+  name: 'Main',
   components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    AppNavigation
+  }
+}
 </script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+      </v-col>      
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'AppNavigation',
+    // data: () => ({
+    // }),
+  }
+</script>
